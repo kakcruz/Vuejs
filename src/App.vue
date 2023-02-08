66c30429fdcec0f85b7eafad2e55f92eb3377dfe
kakcruz <karolinecsantos@hotmail.com>
2023-02-08 16:55:28 -0300

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <PrimeiroComponente />
+    <p>O dado de app é: {{ testando }}</p>
+    <LifeCycle? />
+  </div>
+</template>
+
+<script>
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from "./components/LifeCycle.vue";
+
+export default {
+  name: "App",
+  components: {
+    PrimeiroComponente,
+    LifeCycle,
+  },
+};
+</script>
